64cd3a8f21040d66226099e85ce45b160129d15a
= <=>
2023-07-27 10:08:47 -0300

    assign coins

--- app/Components/Coin/AssignCoins.tsx
+++ app/Components/Coin/AssignCoins.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { useUserContext } from "../../UserContext/UserContext";
 import { useCasinosContext } from "../../CasinoContext/CasinoContext";
-import { useRouter } from "next/router";
+import Link from "next/link";
 import css from "./AssignCoins.module.css";
 import { useUsersContext } from "../../UsersContext/UsersContext";
 
@@ -11,16 +11,16 @@ import { useUsersContext } from "../../UsersContext/UsersContext";
 const AssignCoins = ({setAssigned}) => {
   const { userDb } = useUserContext();
   const { casinosDb } = useCasinosContext();
-  const {charge, setCharge } = useUsersContext(); 
-  
+  const { charge, setCharge } = useUsersContext();
+
   const tokenId = userDb?.token;
   const userLoginId = userDb?.id;
   const [usersCasino, setUsersCasino] = useState(null);
   const [input, setInput] = useState({
     userCasinoId: "",
-    inflow_qty: "",
+    inflow_qty: 0,
   });
-  
+
   const [idCasino, setIdCasino] = useState({
     id: "",
   });
@@ -66,14 +66,20 @@ const AssignCoins = ({setAssigned}) => {
       console.log("data",response) 
 
       if (response.ok) {
-        sweetAlert("Fichas asignadas correctamente");
+        swal({
+          title: "Fichas asignadas correctamente!",
+          icon: "success",
+        });
       } else {
-        sweetAlert("Error al asignar fichas");
+        swal({
+          title: "No se pudieron asignar las fichas",
+          icon: "error",
+        });
       }
     } catch (error) {
       console.log(error.message);
     }
-    reload()
+    reload();
   };
 
   const handleInputChange = ({
@@ -94,6 +100,43 @@ const AssignCoins = ({setAssigned}) => {
     });
   };
 
+  const [errors, setErrors] = useState({
+    id: "",
+    userCasinoId: "",
+    inflow_qty: "",
+  });
+
+  const validateForm = () => {
+    let hasErrors = false;
+    const newErrors = {
+      id: "",
+      userCasinoId: "",
+      inflow_qty: "",
+    };
+
+    if (!idCasino.id || idCasino.id === "Seleccionar Casino") {
+      newErrors.id = "Debes seleccionar un casino.";
+      hasErrors = true;
+    }
+
+    if (!input.userCasinoId || input.userCasinoId === "Seleccionar Usuario") {
+      newErrors.userCasinoId = "Debes seleccionar un usuario.";
+      hasErrors = true;
+    }
+
+    if (input.inflow_qty == 0 || isNaN(input.inflow_qty)|| !input.inflow_qty) {
+      newErrors.inflow_qty = "Cantidad de fichas inválida.";
+      hasErrors = true;
+    } else if (input.inflow_qty < 1) {
+      newErrors.inflow_qty =
+        "La cantidad de fichas debe ser mayor o igual a 1.";
+      hasErrors = true;
+    }
+
+    setErrors(newErrors);
+    return !hasErrors;
+  };
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     postCoins(input, tokenId);
@@ -104,13 +147,13 @@ const AssignCoins = ({setAssigned}) => {
     setAssigned(input.userCasinoId)
   };
 
-
-
   return (
     <div onClick={(e) => e.stopPropagation()} className={css.container}>
       <div className={css.row}>
         <h2>Asignar Fichas</h2>
-        <button >Cerrar</button>
+        <Link className={css.blanco} href="/Admin">
+          cerrar
+        </Link>
       </div>
       <form onSubmit={handleSubmit}>
         <label>Casino:</label>
@@ -120,6 +163,7 @@ const AssignCoins = ({setAssigned}) => {
           value={idCasino.id}
           onChange={handleIdChange}
         >
+          <div className={css.errores}>{errors.id && <p>{errors.id}</p>}</div>
           <option>Seleccionar Casino</option>
           {casinosDb?.map((Cs) => (
             <option key={Cs.id} value={Cs.id}>
@@ -133,6 +177,8 @@ const AssignCoins = ({setAssigned}) => {
           value={input.userCasinoId}
           onChange={handleInputChange}
         >
+          <div className={css.errores}>{errors.userCasinoId && <p>{errors.userCasinoId}</p>}</div>
+          
           <option>Seleccionar Usuario</option>
           {usersCasino?.map((Uc) => (
             <option key={Uc.id} value={Uc.id}>
@@ -146,10 +192,10 @@ const AssignCoins = ({setAssigned}) => {
           type="number"
           name="inflow_qty"
           value={input.inflow_qty}
-          min={1}
           onChange={handleInputChange}
         />
-
+        <div className={css.errores}>{errors.inflow_qty && <p>{errors.inflow_qty}</p>}</div>
+        
         <button type="submit">Asignar fichas</button>
       </form>
     </div>
