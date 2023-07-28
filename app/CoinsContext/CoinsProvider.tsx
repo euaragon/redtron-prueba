bdc1e8e73b5ac002971679f94769131d6147e98a
= <=>
2023-07-28 08:37:53 -0300

    coins provider

--- app/CoinsContext/CoinsProvider.tsx
+++ app/CoinsContext/CoinsProvider.tsx
@@ -1,18 +1,16 @@
-"use client";
 import React, { useEffect } from "react";
-import { CoinsContext } from "./CoinsContext";
+import { CoinsContext, CoinsContextValue } from "./CoinsContext"; // Asegúrate de importar el contexto adecuadamente
 import { useUserContext } from "../UserContext/UserContext";
 import { useCasinosContext } from "../CasinoContext/CasinoContext";
 
 export const CoinsProvider = ({ children, userCasinoId }: any) => {
-  const [coinsDb, setCoinsDB] = React.useState();
+  const [coinsDb, setCoinsDB] = React.useState<Array<any> | null>(null); // Especifiquemos el tipo de 'coinsDb'
   const [charge, setCharge] = React.useState(false);
   const { userDb } = useUserContext();
-  const {casinosDb}=useCasinosContext();
+  const { casinosDb } = useCasinosContext();
   const tokenID = userDb?.token;
-  
 
-  const getCoinsDb = async (userCasinoId) => {
+  const getCoinsDb = async (userCasinoId: string) => {
     try {
       const response = await fetch(`https://redtronapi-development.up.railway.app/coinsMovements?userCasinoId=${userCasinoId}`, {
         headers: {
@@ -28,12 +26,23 @@ export const CoinsProvider = ({ children, userCasinoId }: any) => {
   };
 
   useEffect(() => {
-    tokenID && getCoinsDb(userCasinoId); 
-  }, [userCasinoId]);
- 
+    if (tokenID && userCasinoId) { // Verifiquemos que tanto el tokenID como el userCasinoId tengan valores válidos
+      getCoinsDb(userCasinoId);
+    }
+  }, [tokenID, userCasinoId]);
+
+  // Asegurémonos de que el contexto tenga un valor adecuado si aún no se ha obtenido data
+  const coinsContextValue: CoinsContextValue = {
+    coinsDb,
+    setCoinsDb: setCoinsDB,
+    charge,
+    setCharge,
+    lastMovementInfo: null, 
+    setLastMovementInfo: () => {}
+  };
 
   return (
-    <CoinsContext.Provider value={{ coinsDb, setCharge, charge }}>
+    <CoinsContext.Provider value={coinsContextValue}>
       {children}
     </CoinsContext.Provider>
   );
